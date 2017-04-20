7359e5c9807c57128583a1ca305313dbee5b27c5
Mihai Vultur <mihai.vultur@endava.com>
2017-04-20 06:56:51 +0000

    Fix Berksfile

--- cookbooks/en_deploy_openvpn/metadata.rb
+++ cookbooks/en_deploy_openvpn/metadata.rb
@@ -1,14 +1,15 @@
-name             "en_deploy_openvpn"
-maintainer       "Mihai Vultur"
-maintainer_email "xanto@egaming.ro"
-license          "Apache 2.0"
-description      "Deploy for OpenVPN server"
+name             'en_deploy_openvpn'
+maintainer       'Mihai Vultur'
+maintainer_email 'xanto@egaming.ro'
+license          'Apache 2.0'
+description      'Deploy for OpenVPN server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
 
-%w{redhat centos fedora}.each do |os|
+version          '0.1.0'
+
+%w(redhat centos fedora).each do |os|
   supports os
 end
 
 depends 'openvpn', '~> 3.0'
-depends 'chef-solo-search'
+depends 'chef-solo-search', '~> 0.5'
--- cookbooks/en_deploy_openvpn/recipes/default.rb
+++ cookbooks/en_deploy_openvpn/recipes/default.rb
@@ -3,10 +3,10 @@ include_recipe 'openvpn::server'
 node.default['openvpn']['tar_path'] = '/home/centos/keys'
 link "#{node['openvpn']['key_dir']}/ca.key" do
   to node['openvpn']['signing_ca_key']
-  not_if { ::File.exists? "#{node['openvpn']['key_dir']}/ca.key" }
+  not_if { ::File.exist? "#{node['openvpn']['key_dir']}/ca.key" }
 end
 link "#{node['openvpn']['key_dir']}/ca.crt" do
   to node['openvpn']['signing_ca_cert']
-  not_if { ::File.exists? "#{node['openvpn']['key_dir']}/ca.crt" }
+  not_if { ::File.exist? "#{node['openvpn']['key_dir']}/ca.crt" }
 end
 include_recipe 'openvpn::users'
